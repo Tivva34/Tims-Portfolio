0c0e299c536dee7a8cdf4ca9c28626e98ccbf2dd
Tivva34 <tivva91@gmail.com>
2025-07-07 16:35:39 +0200

    styling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,11 +4,11 @@ import GitHubRepos from "../components/GitHubRepos";
 export default function Projects() {
   return (
      <div className="container">
-    <div className="page projects">
-      <h1>Projects</h1>
-      <p>Here are some of my GitHub projects:</p>
-      <GitHubRepos username="Tivva34" />
-    </div>
+      <div className="page projects">
+        <h1>Projects</h1>
+        <p>Here are some of my GitHub projects:</p>
+        <GitHubRepos username="Tivva34" />
+      </div>
     </div>
   );
 }
