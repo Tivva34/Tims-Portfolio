2908253232f6c7386b850ee41f5aa71d1b4d86f4
Tivva34 <tivva91@gmail.com>
2025-07-10 19:31:38 +0200

    added git path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,14 @@ import sloth from "../assets/sloth.jpg";
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/" className="logo-link">
+      <a
+        href="https://github.com/Tivva34"
+        className="logo-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={sloth} alt="Logo" className="logo-img" />
-      </Link>
+      </a>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
