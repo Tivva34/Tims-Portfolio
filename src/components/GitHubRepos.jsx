4020ed49ea16784f4a604cc96546384ac1868153
Tivva34 <tivva91@gmail.com>
2025-07-10 19:24:11 +0200

    added logos

--- src/components/GitHubRepos.jsx
+++ src/components/GitHubRepos.jsx
@@ -2,6 +2,22 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./GitHubRepos.css";
 
+import jsLogo from "../assets/javascript.svg";
+import tsLogo from "../assets/typescript.svg";
+import reactLogo from "../assets/react.svg";
+import htmlLogo from "../assets/html5.svg";
+import cssLogo from "../assets/css.svg";
+import nodeLogo from "../assets/nodedotjs.svg";
+
+const techIcons = {
+  JavaScript: jsLogo,
+  TypeScript: tsLogo,
+  React: reactLogo,
+  HTML: htmlLogo,
+  CSS: cssLogo,
+  Node: nodeLogo,
+};
+
 const GitHubRepos = ({ username }) => {
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -44,6 +60,15 @@ const GitHubRepos = ({ username }) => {
             >
               View on GitHub
             </a>
+            {repo.language && techIcons[repo.language] && (
+              <div className="tech-icons">
+                <img
+                  src={techIcons[repo.language]}
+                  alt={repo.language}
+                  className="tech-icon"
+                />
+              </div>
+            )}
           </li>
         ))}
       </ul>
