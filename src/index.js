86baa59bab4551ef8205fcf86c21d72ab4d57f2b
Tivva34 <tivva91@gmail.com>
2025-07-13 19:55:50 +0200

    cleaned some code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import cors from 'cors';
 const app = express();
 const PORT = 5000;
 
-// Enable CORS so your React app (on another port) can access this API
 app.use(cors());
 
-// Example API route
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from backend!' });
 });
