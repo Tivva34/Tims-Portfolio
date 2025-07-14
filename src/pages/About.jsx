465a3956b7df330daa56cbf29ea3bd28f5f58d96
Tivva34 <tivva91@gmail.com>
2025-07-14 19:01:46 +0200

    edited about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,8 @@ export default function About() {
         I began programming in 2020 while studying game development, but I soon discovered that my true passion lay in coding itself rather than game design. 
         Since then, I’ve shifted my focus to web development, with a particular interest in frontend technologies like React.
         Currently, I’m studying to become a frontend developer, emphasizing React, creativity, and writing clean, maintainable code. 
-        I love exploring new technologies and turning ideas into interactive, user-friendly experiences.
+        I love exploring new technologies and turning ideas into interactive, user-friendly experiences. 
+        Currently I'm working on an NDA project and can thereby not share any details about it.
       </p>
     </div>
     </div>
