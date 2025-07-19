1a78a6039ed66c61c1d4a68cea1065d7d4c62a00
Tivva34 <tivva91@gmail.com>
2025-07-19 18:19:11 +0200

    added page animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useRef } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-
 import "./App.css";
 
 function App() {
+  const location = useLocation();
+  const nodeRef = useRef(null);
+
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <SwitchTransition>
+        <CSSTransition
+          key={location.pathname}
+          classNames="fade"
+          timeout={350}
+          nodeRef={nodeRef}
+        >
+          <div ref={nodeRef}>
+            <Routes location={location}>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </>
   );
 }
