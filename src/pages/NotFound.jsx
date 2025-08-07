009f66bab30d329136ce840cc7b432b96da516b2
Tivva34 <tivva91@gmail.com>
2025-08-07 18:21:59 +0200

    fixed pages

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,15 +3,51 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-     <div className="container">
-    <div className="page not-found">
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-      <Link to="/">Go back to Home</Link>
-      <Link to="/contact">Contact me if you need help</Link>
-      <p>Or check out my <Link to="/about">About</Link> page to learn more about me.</p>
-      <p>Feel free to explore other sections of the site!</p>
-    </div>
+    <div className="container">
+      <div
+        className="page not-found"
+        style={{
+          textAlign: "center",
+          padding: "3rem 1rem",
+        }}
+      >
+        <span
+          style={{
+            fontSize: "4rem",
+            display: "block",
+            marginBottom: "1rem",
+          }}
+        >
+          😕
+        </span>
+        <h1 style={{ fontSize: "2.5rem", marginBottom: "1rem" }}>
+          404 - Page Not Found
+        </h1>
+        <p style={{ marginBottom: "2rem", color: "#bbb" }}>
+          The page you're looking for doesn't exist.
+        </p>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "1rem",
+            flexWrap: "wrap",
+          }}
+        >
+          <Link to="/" className="btn">
+            Go back to Home
+          </Link>
+          <Link to="/contact" className="btn">
+            Contact me if you need help
+          </Link>
+          <Link to="/about" className="btn">
+            About
+          </Link>
+        </div>
+        <p style={{ marginTop: "2rem", color: "#bbb" }}>
+          Feel free to explore other sections of the site!
+        </p>
+      </div>
     </div>
   );
 }
