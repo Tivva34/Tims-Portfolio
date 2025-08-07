76b2cd50e2c3ff27c1a5fba53e148b31b5001c0b
Tivva34 <tivva91@gmail.com>
2025-08-07 18:24:58 +0200

    notfound fix

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <div className="page not-found">
       <h1>404 - Page Not Found</h1>
       <p>The page you're looking for doesn't exist.</p>
-      <Link to="/">Go back to Home</Link>
+      <p>Go back to <Link to="/">Home</Link></p>
       <p><link to="/contact">Contact</link> me if you habe any questions</p>
       <p>Or check out my <Link to="/about">About</Link> page to learn more about me.</p>
       <p>Feel free to explore other sections of the site!</p>
