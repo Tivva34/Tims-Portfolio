453d876c3994472c6a4c90a58df0334fac914b0e
Tivva34 <tivva91@gmail.com>
2025-08-07 18:31:07 +0200

    small bugfix

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-     <div className="container">
+    <div className="container">
       <div className="page not-found">
         <h1>404 - Page Not Found</h1>
         <p>The page you're looking for doesn't exist.</p>
         <p>Go back to <Link to="/">Home</Link></p>
-        <p><link to="/contact">Contact</link> me if you have any questions</p>
+        <p>Contact me if you have any questions on the <Link to="/contact">Contact</Link> page.</p>
         <p>Or check out my <Link to="/about">About</Link> page to learn more about me.</p>
         <p>Feel free to explore other sections of the site!</p>
       </div>
